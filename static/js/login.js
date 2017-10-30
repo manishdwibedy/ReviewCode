f078abd6127fdeffb4a7ac0f2f8832571307ffd0
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-10-29 20:25:47 -0700

    login is now working.

--- static/js/login.js
+++ static/js/login.js
@@ -1,29 +1,29 @@
 
 // login callback
 function loginCallback(response) {
-if (response.status === "PARTIALLY_AUTHENTICATED") {
-  var code = response.code;
-  var csrf = response.state;
-  document.getElementById("code").value = code;
-  document.getElementById("csrf").value = csrf;
-  document.getElementById("login_success").submit();
-}
-else if (response.status === "NOT_AUTHENTICATED") {
-  document.getElementById("message").innerText = "Not Authenticated";
-}
-else if (response.status === "BAD_PARAMS") {
-  document.getElementById("message").innerText = "Bad Params";
-}
+    if (response.status === "PARTIALLY_AUTHENTICATED") {
+      var code = response.code;
+      var csrf = response.state;
+      document.getElementById("code").value = code;
+      document.getElementById("csrf").value = csrf;
+      document.getElementById("login_success").submit();
+    }
+    else if (response.status === "NOT_AUTHENTICATED") {
+      document.getElementById("message").innerText = "Not Authenticated";
+    }
+    else if (response.status === "BAD_PARAMS") {
+      document.getElementById("message").innerText = "Bad Params";
+    }
 }
 // phone form submission handler
 function smsLogin() {
-// var countryCode = document.getElementById("country_code").value;
-// var phoneNumber = document.getElementById("phone_number").value;
-AccountKit.login(
-  'PHONE',
-  {countryCode: '+1', phoneNumber: ''}, // will use default values if not specified
-  loginCallback
-);
+    var countryCode = document.getElementById("country_code").value;
+    var phoneNumber = document.getElementById("phone_number").value;
+    AccountKit.login(
+      'PHONE',
+      {countryCode: '+1', phoneNumber: ''}, // will use default values if not specified
+      loginCallback
+    );
 }
 // email form submission handler
 function emailLogin() {
