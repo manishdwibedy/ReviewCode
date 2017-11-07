dd4f61d6795b95b06394ae6da1a5f3ec57c07617
Manish Dwibedy <manish.dwibedy@gmail.com>
2017-11-07 10:54:25 -0800

    save to firebase

--- static/js/home.js
+++ static/js/home.js
@@ -1,3 +1,10 @@
+
+var editor = CodeMirror.fromTextArea(document.getElementById("code1"), {
+    lineNumbers: true,
+    matchBrackets: true,
+    mode: $("#lang").val()
+});
+
 $("#stackLogin").click(function(e) {
     e.preventDefault();
     $.ajax({
@@ -12,13 +19,49 @@ $("#stackLogin").click(function(e) {
     });
 });
 
-
-var editor = CodeMirror.fromTextArea(document.getElementById("code1"), {
-    lineNumbers: true,
-    matchBrackets: true,
-    mode: $("#lang").val()
+$("#submit-code").click(function(e) {
+    e.preventDefault();
+    // addData();
 });
+
 
 function switchLanguage() {
     this.editor.setOption("mode", $("#lang").val());
 }
+
+
+function addData(){
+    db.collection("code").add({
+        "user-id": "",
+        "language": $("#lang").val(),
+        "content": this.editor.getValue(),
+        "timestamp": ""
+    })
+    // db.collection("code").add({
+    //     "user-id": "a",
+    //     "language": "bb",
+    //     "content": "c",
+    //     "timestamp": "d"
+    // })
+    .then(function(docRef) {
+        console.log("Document written with ID: ", docRef.id);
+    })
+    .catch(function(error) {
+        console.error("Error adding document: ", error);
+    });
+}
+
+// Initialize Firebase
+var config = {
+apiKey: "AIzaSyAoskz4tPPIuZXRXkZxvzsQ59xbHssTHv0",
+authDomain: "review-code-38d2b.firebaseapp.com",
+databaseURL: "https://review-code-38d2b.firebaseio.com",
+projectId: "review-code-38d2b",
+storageBucket: "",
+messagingSenderId: "585561892876"
+};
+firebase.initializeApp(config);
+
+// Initialize Cloud Firestore through Firebase
+var db = firebase.firestore();
+
